1dbfc4ae67d93691070b08ebb612eeb4a0849dc8
Kathy Snider <sniderk@ohsu.edu>
2020-01-07 10:06:35 -0800

    fixed dir in build script.

--- build-and-zip.sh
+++ build-and-zip.sh
@@ -6,6 +6,8 @@ tarfile=${imagename}_${datestamp}.tar
 pushd /mnt/max/shared/code/internal/utilities/dcan-stack_dockerfiles/${imagename}
 docker build . -t dcanlabs/${imagename}
 popd
+
+pushd /mnt/max/shared/code/internal/utilities/docker_images
 docker save -o ${tarfile} dcanlabs/${imagename}:latest
 chmod g+rw ${tarfile}
 gzip ${tarfile}
